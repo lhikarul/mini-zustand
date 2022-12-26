53005210b8d58cba6031359cb86faa8170e3736f
gateio <gateio@gateiodeMacBook-Pro.local>
2022-12-26 16:53:07 +0800

    feat: shallowEqual

--- src/core/index.ts
+++ src/core/index.ts
@@ -42,10 +42,7 @@ export default function create(fn) {
             );
           }
           // Using functional initial b/c selected itself could be a function
-          if (sliceRef.current !== selected) {
-            // Refresh local slice
-            set(() => selected);
-          }
+          if (!shallowEqual(sliceRef.current, selected)) set(() => selected);
         };
         state.listeners.push(ping);
         return () =>
@@ -64,4 +61,37 @@ export default function create(fn) {
       destory: () => ((state.listeners = []), (state.current = {})),
     },
   ];
+}
+
+const hasOwn = Object.prototype.hasOwnProperty;
+function is(x, y) {
+  if (x === y) {
+    return x !== 0 || y !== 0 || 1 / x === 1 / y;
+  } else {
+    return x !== x && y !== y;
+  }
+}
+function shallowEqual(objA, objB) {
+  if (is(objA, objB)) {
+    return true;
+  }
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+  for (let i = 0; i < keysA.length; i++) {
+    if (!hasOwn.call(objB, keysA[i]) || !is(objA[keysA[i]], objB[keysA[i]])) {
+      return false;
+    }
+  }
+  return true;
 }
